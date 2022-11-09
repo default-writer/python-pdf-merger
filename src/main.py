d156984c41701865608bbe9dc0e42433cb2ba923
default-writer <383256+default-writer@users.noreply.github.com>
2022-11-09 13:16:28 +0300

    Update main.py
    Signed-off-by: default-writer <383256+default-writer@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from PyPDF2 import PdfFileMerger
+#Create a list with the file paths
+from os import listdir, path
+from os.path import isfile, join
+
+
+abs_path = path.abspath(path.join(".","pdf_files"))
+pdf_files = [path.join(abs_path, f) for f in listdir(abs_path) if isfile(join(abs_path, f))]
 
 #Create an instance of PdfFileMerger() class
-merger = PdfFileMerger()
-
-#Create a list with the file paths
-pdf_files = ['pdf_files/file1.pdf', 'pdf_files/file2.pdf']
+merger = PdfFileMerger(strict=False)
 
 #Iterate over the list of the file paths
 for pdf_file in pdf_files:
